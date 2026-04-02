561f958cf43217ec2adb7ec5d851d0b35c8282ff
dvyenky <dvyenkatesh@gmail.com>
2026-04-02 23:45:10 +0530

    NetFlix SetUP

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-black">
+        <h1 className="text-4xl font-bold text-green-400">Tailwind Working</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
